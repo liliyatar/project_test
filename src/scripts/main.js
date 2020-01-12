d738eb3602352e523b1eb3c3ceb9676aabb6b2f2
Liliya Taran <liliyataran2017@gmail.com>
2020-01-12 17:15:29 +0200

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
+// eslint-disable-next-line no-undef
+$(document).ready(function() {
+  // eslint-disable-next-line no-undef
+  $('.menu__trigger').click(function() {
+    // eslint-disable-next-line no-undef
+    $('#menu').slideToggle(500);
+  });
+
+  // eslint-disable-next-line no-undef
+  $(window).resize(function() {
+    // eslint-disable-next-line no-undef
+    if ($(window).width() > 800) {
+      // eslint-disable-next-line no-undef
+      $('#menu').removeAttr('style');
+    }
+  });
+});
+
 (function() {
   setTimeout(function() {
     document.querySelector('header')
